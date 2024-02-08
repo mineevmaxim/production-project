ddf6788018812a48f2fdf5d798e471fe03fafc82
Maxim Mineev <mms1401@ya.ru>
2024-02-08 23:42:24 +0500

    webpack dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,10 +3,11 @@ import webpack from "webpack";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
 
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
 
     return {
         mode: mode,
@@ -21,5 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map': undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {someFn} from "./test";
 
 someFn(21);
+
+document.body.innerHTML = `<div>Hello World!</div>`
