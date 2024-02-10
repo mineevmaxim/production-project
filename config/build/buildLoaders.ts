f3daaaab9bf848dc36225b1e70828ffbaf7545af
Maxim Mineev <mms1401@ya.ru>
2024-02-10 16:35:13 +0500

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -43,7 +43,29 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
+    const babelLoader = {
+        test: /\.(js|tsx|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            "nsSeparator": "~",
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
