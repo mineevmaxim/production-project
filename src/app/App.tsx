d33c7fd91d87c3cfceb5e712297e946e612816a5
Maxim Mineev <mms1401@ya.ru>
2024-02-10 16:10:29 +0500

    i18n internationalisation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,22 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {Suspense} from "react";
+import {useTranslation} from "react-i18next";
 
 const App = () => {
     const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback={'loading...'}>
+                <Navbar/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
+
         </div>
     );
 };
---
+++ src/shared/ui/LangSwitcher/index.ts
@@ -0,0 +1,5 @@
+import {LangSwitcher} from "shared/ui/LangSwitcher/ui/LangSwitcher";
+
+export {
+    LangSwitcher,
+}
