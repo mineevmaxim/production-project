1babd5f8488b85ca7213a2b12a9fdfd703310a54
Maxim Mineev <mms1401@ya.ru>
2024-02-13 21:17:24 +0500

    add webpack bundle analyzer

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -1,5 +1,4 @@
 import React, { ErrorInfo, ReactNode, Suspense } from 'react';
-import { withTranslation } from 'react-i18next';
 import { ErrorPage } from 'widgets/ErrorPage/ui/ErrorPage';
 import { PageLoader } from 'widgets/PageLoader';
 
