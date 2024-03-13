a3fe3c6bef54ff03ec5d61599264987ddf6dd59d
Maxim Mineev <mms1401@ya.ru>
2024-03-13 23:07:34 +0500

    add type to query params

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
@@ -7,7 +7,7 @@ import {
 } from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
 import { articlesPageActions } from 'pages/ArticlesPage/model/slices/articlesPageSlice';
 import { fetchArticlesList } from 'pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList';
-import { ArticleSortField } from 'entities/Article';
+import { ArticleSortField, ArticleType } from 'entities/Article';
 import { SortOrder } from 'shared/types';
 
 export const initArticlesPage = createAsyncThunk<
@@ -24,6 +24,7 @@ export const initArticlesPage = createAsyncThunk<
             const orderFromUrl = searchParams.get('order') as SortOrder;
             const sortFromUrl = searchParams.get('sort') as ArticleSortField;
             const searchFromUrl = searchParams.get('search');
+            const typeFromUrl = searchParams.get('type') as ArticleType;
 
             if (orderFromUrl) {
                 dispatch(articlesPageActions.setOrder(orderFromUrl));
@@ -33,6 +34,9 @@ export const initArticlesPage = createAsyncThunk<
             }
             if (searchFromUrl) {
                 dispatch(articlesPageActions.setSearch(searchFromUrl));
+            }
+            if (typeFromUrl) {
+                dispatch(articlesPageActions.setType(typeFromUrl));
             }
 
             dispatch(articlesPageActions.initState());
--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -68,8 +68,8 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
     const onChangeType = useCallback((type: ArticleType) => {
         dispatch(articlesPageActions.setType(type));
         dispatch(articlesPageActions.setPage(1));
-        debouncedFetchData();
-    }, [dispatch, debouncedFetchData]);
+        fetchData();
+    }, [dispatch, fetchData]);
 
     return (
         <div className={classNames(cls.ArticlesPageFilters, {}, [className])}>
