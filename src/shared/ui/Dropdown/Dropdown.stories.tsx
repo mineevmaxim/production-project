8720f7355a4a26533f76c7910e8b679ed8fe5445
Maxim Mineev <mms1401@ya.ru>
2024-03-17 23:39:24 +0500

    fix codestyle

--- src/shared/ui/Dropdown/Dropdown.stories.tsx
+++ src/shared/ui/Dropdown/Dropdown.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from 'shared/ui/Button/Button';
+import { Button } from '../Button/Button';
 import { Dropdown } from './Dropdown';
 
 export default {
--- src/shared/ui/Dropdown/Dropdown.tsx
+++ src/shared/ui/Dropdown/Dropdown.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Fragment, memo, ReactNode } from 'react';
 import { Menu } from '@headlessui/react';
 import { DropdownDirection } from 'shared/types/ui';
-import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { AppLink } from '../AppLink/AppLink';
 import cls from './Dropdown.module.scss';
 
 export interface DropdownItem {
