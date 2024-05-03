17ef27238f8ed6f6f92306d38800d4d879a84832
Maxim Mineev <mms1401@ya.ru>
2024-05-03 11:07:52 +0500

    clear main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -15,11 +15,6 @@ const MainPage = () => {
     return (
         <Page>
             {t('Главная страница')}
-            <RatingCard
-                title="Как вам статья?"
-                feedbackTitle="Оставьте отзыв о статье"
-                hasFeedback
-            />
         </Page>
     );
 };
