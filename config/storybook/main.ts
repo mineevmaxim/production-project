defa67716d8531c9adf745eff43785ae36dc9196
Maxim Mineev <mms1401@ya.ru>
2024-05-05 20:08:36 +0500

    ugrade storybook, fix packages

---
+++ config/storybook/main.ts
@@ -0,0 +1,67 @@
+import webpack, { RuleSetRule, DefinePlugin } from 'webpack';
+import path from 'path';
+import { BuildPaths } from '../build/types/config';
+
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
+
+export default {
+    stories: [
+        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                backgrounds: false,
+            },
+        },
+        '@storybook/addon-interactions',
+        'storybook-addon-mock/register',
+        'storybook-addon-themes',
+    ],
+    framework: '@storybook/react',
+    core: {
+        builder: 'webpack5',
+    },
+    webpackFinal: async (config: webpack.Configuration) => {
+        const paths: BuildPaths = {
+            build: '',
+            html: '',
+            entry: '',
+            src: path.resolve(__dirname, '..', '..', 'src'),
+            locales: '',
+            buildLocales: '',
+        };
+        config!.resolve!.modules!.push(paths.src);
+        config!.resolve!.extensions!.push('.ts', '.tsx');
+        config!.resolve!.alias = {
+            ...config!.resolve!.alias,
+            '@': paths.src,
+        };
+
+        // eslint-disable-next-line no-param-reassign
+        // @ts-ignore
+        config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
+            if (/svg/.test(rule.test as string)) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
+
+            return rule;
+        });
+
+        config!.module!.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        config!.module!.rules.push(buildCssLoader(true));
+
+        config!.plugins!.push(new DefinePlugin({
+            __IS_DEV__: JSON.stringify(true),
+            __API__: JSON.stringify('https://testapi.ru'),
+            __PROJECT__: JSON.stringify('storybook'),
+        }));
+
+        return config;
+    },
+};
