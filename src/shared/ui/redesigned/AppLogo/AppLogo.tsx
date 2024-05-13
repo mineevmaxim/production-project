1f4df9f3e85e657e0a0fd170b512eb20996960ad
Maxim Mineev <mms1401@ya.ru>
2024-05-13 12:56:39 +0500

    mini fixes

--- src/shared/ui/redesigned/AppLogo/AppLogo.tsx
+++ src/shared/ui/redesigned/AppLogo/AppLogo.tsx
@@ -16,14 +16,14 @@ export const AppLogo = memo(({ className, size = 50 }: AppLogoProps) => {
 			justify="center"
 			className={classNames(cls.appLogoWrapper, {}, [className])}
 		>
-			<div className={cls.gradientBig} />
-			<div className={cls.gradientSmall} />
 			<AppSvg
 				className={cls.appLogo}
 				width={size}
 				height={size}
 				color="black"
 			/>
+			<div className={cls.gradientBig} />
+			<div className={cls.gradientSmall} />
 		</HStack>
 	);
 });
