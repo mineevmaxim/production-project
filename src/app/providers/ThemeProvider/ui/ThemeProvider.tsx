302b7c53c8eefdb885f45504c890cc256e45eb13
Maxim Mineev <mms1401@ya.ru>
2024-05-14 23:16:31 +0500

    fix skeleton size, fix scrollbar

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -4,39 +4,43 @@ import { Theme } from '@/shared/const/theme';
 import { useJsonSettings } from '@/entities/User';
 
 interface ThemeProviderProps {
-    initialTheme?: Theme;
-    children: ReactNode;
+	initialTheme?: Theme;
+	children: ReactNode;
 }
 
 const ThemeProvider = (props: ThemeProviderProps) => {
-    const { initialTheme, children } = props;
-    const { theme: defaultTheme } = useJsonSettings();
-    const [isThemeInited, setThemeInited] = useState(false);
+	const { initialTheme, children } = props;
+	const { theme: defaultTheme } = useJsonSettings();
+	const [isThemeInited, setThemeInited] = useState(false);
 
-    const [theme, setTheme] = useState<Theme>(
-        initialTheme || defaultTheme || Theme.LIGHT,
-    );
+	const [theme, setTheme] = useState<Theme>(
+		initialTheme || defaultTheme || Theme.LIGHT,
+	);
 
-    useEffect(() => {
-        if (!isThemeInited && defaultTheme) {
-            setTheme(defaultTheme);
-            setThemeInited(true);
-        }
-    }, [defaultTheme, isThemeInited]);
+	useEffect(() => {
+		if (!isThemeInited && defaultTheme) {
+			setTheme(defaultTheme);
+			setThemeInited(true);
+		}
+	}, [defaultTheme, isThemeInited]);
 
-    const defaultProps = useMemo(
-        () => ({
-            theme,
-            setTheme,
-        }),
-        [theme],
-    );
+	useEffect(() => {
+		document.body.className = theme;
+	}, [theme]);
 
-    return (
-        <ThemeContext.Provider value={defaultProps}>
-            {children}
-        </ThemeContext.Provider>
-    );
+	const defaultProps = useMemo(
+		() => ({
+			theme,
+			setTheme,
+		}),
+		[theme],
+	);
+
+	return (
+		<ThemeContext.Provider value={defaultProps}>
+			{children}
+		</ThemeContext.Provider>
+	);
 };
 
 export default ThemeProvider;
