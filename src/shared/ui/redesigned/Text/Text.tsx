f8534d76a007a19c790446bd300a87ac5360326e
Maxim Mineev <mms1401@ya.ru>
2024-05-14 16:20:21 +0500

    outline btn, list view article, skeleton

--- src/shared/ui/redesigned/Text/Text.tsx
+++ src/shared/ui/redesigned/Text/Text.tsx
@@ -15,6 +15,7 @@ interface TextProps {
 	variant?: TextVariant;
 	align?: TextAlign;
 	size?: TextSize;
+	bold?: boolean;
 
 	'data-testid'?: string;
 }
@@ -35,6 +36,7 @@ export const Text = memo((props: TextProps) => {
 		variant = 'primary',
 		align = 'left',
 		size = 'm',
+		bold,
 		'data-testid': dataTestId = 'Text',
 	} = props;
 
@@ -42,7 +44,7 @@ export const Text = memo((props: TextProps) => {
 
 	return (
 		<div
-			className={classNames(cls.Text, {}, [
+			className={classNames(cls.Text, { [cls.bold]: bold }, [
 				className,
 				cls[variant],
 				cls[align],
